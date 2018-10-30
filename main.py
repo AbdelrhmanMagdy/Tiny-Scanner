d59e4185d0e61c2337f45fb7568994434caee594
Abdelrhman Magdy <abdelrhman.magdy96@gmail.com>
2018-10-30 23:06:35 +0200

    fix \n bug in token classifier

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from scanner import scanner
 import sys
 
 def main():
-    file_name = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
+    file_name = sys.argv[1] if len(sys.argv)>1 else 'sample_input.txt'
     obj = scanner()
     obj.scan(file_name)
     obj.output()
